f2b10f815c5acc789a1b4797bc0731d8e6563228
AntoniIwinski <145766013+AntoniIwinski@users.noreply.github.com>
2024-01-31 19:19:16 +0100

    Add files via upload

---
+++ 11.01/index.php
@@ -0,0 +1,12 @@
+<?php
+//Praca zainspirowana od Kamila Chęcińskiego
+
+require_once 'db.php';
+
+$database = new Database('127.0.0.1', 'root', '', 'grupa_s');
+
+var_dump($database->getByPrimaryKey("products", 1, 1));
+
+var_dump($database->getAllData("users"));
+
+var_dump($database->getByUniqueIndex("products", 2,2));
